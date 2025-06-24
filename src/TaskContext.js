eaad46500bd4032a683be60a9f638cb72290fc3b
adam1ak <f.admiakk@gmail.com>
2025-06-24 09:49:09 +0200

    Implemented receiving and deleting tasks from db

--- src/TaskContext.js
+++ src/TaskContext.js
@@ -1,18 +1,14 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import { db } from "./firebaseConfig"
-import { setDoc, doc, onSnapshot, collection, addDoc } from "firebase/firestore";
+import { doc, onSnapshot, collection, deleteDoc, addDoc, query, orderBy, where, getDocs } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { PropagateLoader } from 'react-spinners';
 
-
 const TaskContext = createContext();
 
-
 export function TaskProvider({ children }) {
-
   const [modalFunction, setModalFunction] = useState(null);
   const [currentTask, setCurrentTask] = useState(null);
-
   const [currentUser, setCurrentUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(true);
 
@@ -27,29 +23,46 @@ export function TaskProvider({ children }) {
     return unsubscribe;
   }, [auth])
 
-  const [tasks, setTasks] = useState(() => {
-    try {
-      const saved = localStorage.getItem('tasks');
-      return saved ? JSON.parse(saved) : [];
-    } catch (error) {
-      console.log("Failed to parse tasks from localStorage", error);
-      return [];
-    }
-  });
+  const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }, [tasks]);
+    if (!currentUser?.uid) return;
 
+    const fetchTasks = () => {
+      try {
+        const taskRef = collection(db, "users", currentUser.uid, "tasks");
 
-  // Reciving user data from localstorage
-  // If there is no existing data
-  // It will try to recive from database ( firebase )
+        const unsubscribe = onSnapshot(taskRef, (querySnapshot) => {
+          const newTasks = querySnapshot.docs.map((doc) => ({ ...doc.data() }));
+
+          setTasks((prevTask) => {
+            if (JSON.stringify(prevTask) !== JSON.stringify(newTasks)) {
+              return newTasks;
+            } else {
+              return prevTask;
+            }
+          })
+
+          console.log("Succed to recive tasks from database")
+        });
+        return unsubscribe;
+      } catch (error) {
+        console.error("Failed to receive tasks from database", error)
+      }
+    };
+
+    fetchTasks();
+  }, [currentUser?.uid])
+
 
   const [userInfo, setUserInfo] = useState(() => {
     try {
       const saved = localStorage.getItem('userInfo');
-      return saved ? JSON.parse(saved) : null;
+      if (saved) {
+        setLoadingAuth(false);
+        return JSON.parse(saved);
+      }
+      return null;
     } catch (error) {
       console.error("Failed to parse user info from localStorage", error);
       return null;
@@ -61,9 +74,10 @@ export function TaskProvider({ children }) {
 
     const userRef = doc(db, "users", currentUser?.uid);
     const unsub = onSnapshot(userRef, (doc) => {
-      if (doc.exists) {
+      if (doc.exists()) {
         const data = doc.data();
         setUserInfo(data)
+        setLoadingAuth(false);
         localStorage.setItem("userInfo", JSON.stringify(data));
       }
     })
@@ -71,7 +85,10 @@ export function TaskProvider({ children }) {
     return unsub;
   }, [currentUser?.uid, userInfo])
 
-  // Setting userInfo to localStorage in AuthForm.js component.
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
   const handleSetUserInfo = (data) => {
     try {
       localStorage.setItem('userInfo', JSON.stringify(data));
@@ -90,16 +107,21 @@ export function TaskProvider({ children }) {
   }
 
   const addTask = async (newTask) => {
-    setTasks([...tasks, { ...newTask, id: Date.now() }]);
+    if (!currentUser?.uid) {
+      console.error("Cannot add task: No user logged in.");
+      return;
+    }
+
+    const taskWithId = { ...newTask, id: Date.now() };
+    setTasks([...tasks, taskWithId]);
+
     try {
-      const docId = `${Date.now()}-${currentUser?.uid}`;
-      const docRef = doc(db, "tasks", docId);
-
-      await setDoc(docRef, {
-        ...newTask
-      })
-      console.log("Document wrriten to database")
-
+      const userTasksRef = collection(db, "users", currentUser.uid, "tasks");
+      await addDoc(userTasksRef, {
+        ...newTask,
+        id: Date.now(),
+      });
+      console.log("Document written to database");
     } catch (e) {
       console.error("Error adding document: ", e);
     }
@@ -121,14 +143,27 @@ export function TaskProvider({ children }) {
     ));
   };
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
     setTasks(tasks.filter(task => task.id !== id));
-  };
+    try {
+      const taskRef = collection(db, "users", currentUser.uid, "tasks");
+      const q = query(taskRef, where("id", "==", id));
+      const querySnapshot = await getDocs(q);
 
-  if (!loadingAuth) {
+      querySnapshot.forEach(async (doc) => {
+        await deleteDoc(doc.ref);
+        console.log("Document deleted form database");
+      })
+
+    } catch (error) {
+      console.error("Error during deleting task", error)
+    }
+  };
+  
+  if (loadingAuth) {
     return (
-      <div className="flex items-center justify-center w-full h-full">
-        <div className="text-center">
+      <div className="flex bg-yellow-300 items-center justify-center w-full h-full">
+        <div className="text-center bg-yellow-300">
           <PropagateLoader
             color="#16A34A"
             loading
